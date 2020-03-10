97dd93ebb9a5a6d509d3f772732c4447c98336d2
thevibration <harshabar9@gmail.com>
2020-03-10 17:04:23 -0400

    probwith7

--- assignment2/assignment2/src/lambda.ml
+++ assignment2/assignment2/src/lambda.ml
@@ -88,7 +88,7 @@ let rec substitute expr a b =
     else
       let z = fresh "z" in 
       let e0' = (substitute (e0) (x) (Var z)) in (Lam (z,(substitute e0' a b)))
-
+(*
 let rec reduce_cbv e =
   (* YOUR CODE HERE *)
   raise (Failure "Problem 7 not implemented")
@@ -98,13 +98,45 @@ let rec reduce_cbv e =
 let rec reduce_cbn e =
   (* YOUR CODE HERE *)
   raise (Failure "Problem 8 not implemented")
-
-
+*)
 
 let rec reduce_normal e =
-  (* YOUR CODE HERE *)
-  raise (Failure "Problem 9 not implemented")
-
+  let rec substitute expr a b =
+    let rec free_variables e =
+      let union l1 l2 =
+        let rec remove_stutter l =
+          match l with
+          | [] -> []
+          | x::y::tail when x=y -> remove_stutter (y::tail)
+          | x::tail -> x::remove_stutter tail
+        in
+        remove_stutter((List.sort String.compare (l1@l2)))
+      in
+      match e with
+      | Var x -> [x]
+      | App (e1,e2) -> union (free_variables e1) (free_variables e2)
+      | Lam (x,e0) -> remove x (free_variables e0)
+    in
+    let mem e l = List.fold_left(fun acc x -> if x=e then true else acc) false l
+    in
+    match expr with
+    | Var x -> if a=x then b else expr
+    | App (e1,e2) -> App (substitute e1 a b, substitute e2 a b)
+    | Lam (x,e0) -> 
+      if a=x then expr
+      else if not (mem x (free_variables b)) then Lam (x, substitute e0 a b)
+      else
+        let z = fresh "z" in 
+        let e0' = (substitute (e0) (x) (Var z)) in (Lam (z,(substitute e0' a b)))
+  in
+  match e with
+  | App (Lam (x,e),e2) -> (substitute e x e2,true)
+  | App (e1,e2) ->
+    let e1' = reduce_normal e1 in
+    if e1' <> (e1,true) then ((App (e1',e2)),true)
+    else ((App (e1,reduce_normal e2)),true)
+  | Lam (x,e) -> ((Lam (x, reduce_normal e)),true)
+  | _ -> (e,false) 
 
 (* Your implementation done here *)
 
@@ -118,8 +150,8 @@ let rec eval log depth reduce expr =
       eval log (depth-1) reduce expr'
     end
   end
-let eval_cbv = eval true 1000 reduce_cbv
-let eval_cbn = eval true 1000 reduce_cbn
+(*let eval_cbv = eval true 1000 reduce_cbv*)
+(*let eval_cbn = eval true 1000 reduce_cbn*)
 let eval_normal = eval true 1000 reduce_normal
 
 (* To debug and observe the evaluation steps of your `reduce_cbv`, `reduce_cbn`
